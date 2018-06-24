5177cbd0bd5bd23430cabaed7a2b2154f9ec327a
Tucker Bradford <github@tuckerbradford.com>
2018-06-24 13:15:39 -0400

    changed joins to includes for podcast_controller

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -70,7 +70,7 @@ class PodcastsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_podcast
-      @podcast = Podcast.joins(:episodes).find(params[:id])
+      @podcast = Podcast.includes(:episodes).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
