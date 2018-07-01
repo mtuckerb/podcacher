7876c2e42b185016edb692865d1ec3d09943fbcc
Tucker Bradford <github@tuckerbradford.com>
2018-07-01 11:25:48 -0400

    uses podcast id rather than instance for job

--- app/jobs/update_podcast_job.rb
+++ app/jobs/update_podcast_job.rb
@@ -2,7 +2,8 @@ class UpdatePodcastJob < ApplicationJob
 
   queue_as :default
 
-  def perform(podcast:)
+  def perform(podcast_id:)
+    podcast = Podcast.find podcast_id
     logger.info "checking #{podcast} for new episodes"
     podcast.download_new_episodes
   end
